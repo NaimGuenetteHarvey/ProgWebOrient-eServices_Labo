53e0c68472ea491b6e277a6edf6e83f822453563
Naïm Guénette-Harvey <2360188@cegepmontpetit.ca>
2026-02-16 14:41:37 -0500

    FCT:Étape 5 fait

--- labo5/app/details/page.tsx
+++ labo5/app/details/page.tsx
@@ -1,9 +1,34 @@
 'use client'; // Seulement nécessaire pour les composants interactifs
-
+import { useEffect, useState } from "react";
+import { Character } from "../_types/character";;
+import axios from "axios";
+import { useParams } from "next/navigation";
 export default function details() {
 
-  const characterName : string | null = null
-  const characterDetails : Character | null = null
+   const [characterName, setCharacterName] = useState<string|null>(null);
+   const [characterDetails, setCharacterDetails] = useState<Character|null>(null);
+   const params = useParams<{ characterName : string[] }>();
+     useEffect(() => {
+
+        // Est-ce qu'un paramètre de route a été reçu ? Si non, on utilise "kenny"
+        const name = params.characterName ? params.characterName[0] : "kenny";
+
+        setCharacterName(name);
+        getCharacter(name);
+
+    }, []);
+
+
+  async function getCharacter(name : string){
+
+     const response = await axios.get("https://spapi.dev/api/characters?search=" + name)
+     console.log(response.data);
+     const json = response.data.data[0];
+
+    setCharacterDetails(new Character(json.name, json.age, json.occupation, json.grade, json.episodes.length));
+
+  }
+
   return (
     <div>
     <h3>Détails sur ???</h3>
