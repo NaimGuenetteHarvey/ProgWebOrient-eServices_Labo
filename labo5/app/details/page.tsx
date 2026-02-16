f2e7990c653a6bd4539ad7f96f45fbee428faf86
Naïm Guénette-Harvey <2360188@cegepmontpetit.ca>
2026-02-16 14:16:28 -0500

    FCT: Début de l'étape 4

--- labo5/app/details/page.tsx
+++ labo5/app/details/page.tsx
@@ -1,8 +1,25 @@
 'use client'; // Seulement nécessaire pour les composants interactifs
 
-export default function NomDuComposant() {
+export default function details() {
 
+  const characterName : string | null = null
+  const characterDetails : Character | null = null
   return (
-    <div></div>
+    <div>
+    <h3>Détails sur ???</h3>
+    <img src="/images/???.png" alt="???" />
+
+    <div>
+        <table>
+            <tbody>
+                <tr><td><b>Nom complet</b> : </td><td>???</td></tr>
+                <tr><td><b>Âge</b> : </td><td>???</td></tr>
+                <tr><td><b>Occupation</b> : </td><td>???</td></tr>
+                <tr><td><b>Grade</b> : </td><td>???</td></tr>
+                <tr><td><b>Nombre d'épisodes</b> : </td><td>???</td></tr>
+            </tbody>
+        </table>
+    </div>
+</div>
   );
 }
